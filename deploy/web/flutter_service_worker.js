dbf485951073903ccef1adde42e6598d563047c2
darryn.campbell <darryn.campbell@pubnub.com>
2022-08-09 15:15:45 +0100

    test update

--- deploy/web/flutter_service_worker.js
+++ deploy/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f03338242f225ce96140c1aaa7eba526",
-"index.html": "804741260eb3fa8f8f07c9ca59f95b07",
-"/": "804741260eb3fa8f8f07c9ca59f95b07",
+"index.html": "c81e8c2b72f2caa80a49b80f00a5caee",
+"/": "c81e8c2b72f2caa80a49b80f00a5caee",
 "features.json": "082b76bfb8ab2eb2aaf173cdc677fd9e",
 "main.dart.js": "1ea0e1e3192958e617ad3522db380c1b",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
